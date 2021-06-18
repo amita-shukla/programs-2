879f61c754678bc334e429c08abc7f797685f60d
Amita Shukla <amitashukla0906@gmail.com>
2021-06-19 03:26:19 +0530

    topological sort & cycle detection

--- src/main/java/in/amita/practice/GraphCourseScheduleII.java
+++ src/main/java/in/amita/practice/GraphCourseScheduleII.java
@@ -1,4 +1,8 @@
+package in.amita.practice;
+
+
 import java.util.*;
+
 /*
 There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
 For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
@@ -19,6 +23,7 @@ Example 3:
 Input: numCourses = 1, prerequisites = []
 Output: [0]
  */
+
 public class GraphCourseScheduleII {
     Graph g;
     state[] visited;
